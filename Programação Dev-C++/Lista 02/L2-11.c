82618adc660201e50e321a06728e451eca94e878
Lucas Moraes <lucas.panik@gmail.com>
2015-09-26 13:42:02 -0300

    Lista 02

---
+++ Programação Dev-C++/Lista 02/L2-11.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main(){
+	int i;
+	for(i = 0;i < 256; i++){
+		printf("%d \t %c \n", i, i);
+	}
+}
---
+++ Programação Dev-C++/README.md
@@ -0,0 +1,2 @@
+﻿## Programação C / Dev-C++
+Algoritimos de C/C++ para aprendizado do 2º Semestre UNISUAM/RJ-BONSUCESSO

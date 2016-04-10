9019292696aeb91933db3404ca40de6d7828b88c
Lucas Moraes (Panik) <lucas.panik@gmail.com>
2016-04-10 20:43:31 -0300

    Update ExeVet21.java

--- 3º Semestre/POO/Lista Vetor 01/ExeVet21.java
+++ 3º Semestre/POO/Lista Vetor 01/ExeVet21.java
@@ -2,13 +2,18 @@ import java.util.Scanner;
 
 class ExeVet21 {
     public static void main(String args[]) {
-        Scanner s new Scanner(System.in);
-        double temp[] = new double[121];
-        int i, numeroDeDias;
+        Scanner s = new Scanner(System.in);
+        double temp[] = new double[5];
+        int numeroDeDias;
         double menorTemp, maiorTemp, tempMedia,somaTemp;
         
-        somaTemp = 0.0;
-        for(i = 0; i < temp.length; i++){
+        somaTemp = 0;
+        menorTemp = 0.0;
+        maiorTemp = 0;
+        tempMedia = 0;
+        somaTemp = 0;
+        
+        for(int i = 0; i < temp.length; i++){
             System.out.print("Digite uma temperatura: ");
             temp[i] = s.nextDouble();
             // Menor e Maior
@@ -28,13 +33,13 @@ class ExeVet21 {
         tempMedia = somaTemp / temp.length;
         numeroDeDias = 0;
         
-         for(i = 0; i < temp.length; i++){
+         for(int i = 0; i < temp.length; i++){
             if(temp[i] < tempMedia)
                 numeroDeDias++;
         }
         System.out.println("Menor temperatura: " + menorTemp);
         System.out.println("Maior temperatura: " + maiorTemp);
-        System.out.println("Temperatura m?ia: " + tempMedia);
-        System.out.println("Número de dias: " + numeroDeDias);
+        System.out.println("Temperatura média: " + tempMedia);
+        System.out.println("Número de dias inferior: " + numeroDeDias);
     }
 }
